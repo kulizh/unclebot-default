dc1b5711a8c7ebb4dbfc938b4386d9bfcc939e27
Nikita Kulizhnikov <mail@kulizh.ru>
2024-07-03 13:37:16 +0300

    Log exceptions

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,18 +1,23 @@
 <?php
 require_once dirname(__FILE__) . '/../data/vendor/autoload.php';
-error_reporting(0);
+
+use \Unclebot\Utils\Logger;
 
 // project scope functions
 function ppd($array)
 {
-	echo '<pre>';
-	print_r($array);
-	die('</pre>');
+    echo '<pre>';
+    print_r($array);
+    die('</pre>');
 }
 
 // const
 define('FILES_DIR', realpath(dirname(__FILE__) . '/../data/files/') . '/');
 
-// run router
-$router = new Unclebot\Router;
-$router->handle();
+try {
+    $router = new Unclebot\Router;
+    $router->handle();
+} catch (Exception $exception) {
+    $logger = new Logger('errors');
+    $logger->write($exception);
+}
